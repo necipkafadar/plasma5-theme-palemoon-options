0b5058a9ab9df9dc4d291c32a5dc86421b552ee9
Necip Kafadar <necipkafadar@gmail.com>
2016-03-24 22:40:48 +0200

    registered sheet control

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -44,7 +44,8 @@ var plasma5ThemeOptions = {
 		var ios = Components.classes["@mozilla.org/network/io-service;1"]
 	                   .getService(Components.interfaces.nsIIOService);
 		var uri = ios.newURI("chrome://browser/skin/subskins/"+sheetName+".css", null, null);
-						sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+		if(!sss.sheetRegistered(uri, sss.USER_SHEET))
+			sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
 	},
 	
 }
