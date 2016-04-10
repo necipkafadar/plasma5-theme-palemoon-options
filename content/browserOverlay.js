1e331b7ace3fe63589a21825a1090b7b84e86eeb
Necip Kafadar <necipkafadar@gmail.com>
2016-04-10 16:14:35 +0300

    color picker for label-description colors

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -1,6 +1,8 @@
 var plasma5ThemeOptions = {
 	prefs: null,
 	colorScheme: "",
+	labelTextColor: "",
+	labelTextColor_att:"",
 
 	// Initialize the extension
 
@@ -13,6 +15,12 @@ var plasma5ThemeOptions = {
 		
 		this.colorScheme = this.prefs.getCharPref("colorScheme");
 		this.applyCSS(this.colorScheme);
+
+
+		this.labelTextColor = this.prefs.getBoolPref("labelTextColor");
+		this.labelTextColor_att = this.prefs.getCharPref("labelTextColor_att");
+		if(this.labelTextColor==true) 
+			this.applyCSSData("labelTextColor", this.labelTextColor_att);
     },
 	
 	observe: function(subject, topic, data) {
@@ -27,13 +35,28 @@ var plasma5ThemeOptions = {
 				this.colorScheme = this.prefs.getCharPref("colorScheme");
 				this.applyCSS(this.colorScheme);
 				break;
+			case "labelTextColor":
+				this.labelTextColor = this.prefs.getBoolPref("labelTextColor");
+				if(this.labelTextColor==true) {
+					this.labelTextColor_att = this.prefs.getCharPref("labelTextColor_att");
+					this.applyCSSData("labelTextColor", this.labelTextColor_att);
+				}
+				else {
+					if(this.colorScheme == "plasma5-dark")
+						this.applyCSSData("labelTextColor", "#eff0f1");
+					else
+						this.applyCSSData("labelTextColor", "#3D3D3D");
+				}
+				break;
+			case "labelTextColor_att":
+				if(this.labelTextColor==true) {
+					this.labelTextColor_att = this.prefs.getCharPref("labelTextColor_att");
+					this.applyCSSData("labelTextColor", this.labelTextColor_att);
+				}
+				break;
 		}
 	},
-	
-	watchColorScheme: function(newColorScheme) {
-		this.prefs.setCharPref("colorScheme", newColorScheme);
-	},
-	
+
  	onMenuItemCommand: function(e) {
 		window.openDialog("chrome://plasma5-theme-palemoon-options/content/options.xul","chrome, toolbar, dialog, resizable=no").focus();
 	},
@@ -46,6 +69,24 @@ var plasma5ThemeOptions = {
 		var uri = ios.newURI("chrome://browser/skin/subskins/"+sheetName+".css", null, null);
 		if(!sss.sheetRegistered(uri, sss.USER_SHEET))
 			sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+	},
+
+	applyCSSData: function (item, color) {
+		var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"]
+	                   .getService(Components.interfaces.nsIStyleSheetService);
+		var ios = Components.classes["@mozilla.org/network/io-service;1"]
+	                   .getService(Components.interfaces.nsIIOService);
+	    switch(item) {
+	    	case "labelTextColor":
+	    		var uri = ios.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
+	    			description, label {\
+  						color: '+color+' !important;\
+						}\
+	    			'), null, null);
+	    	break;
+	    }
+	    if(!sss.sheetRegistered(uri, sss.USER_SHEET))
+			sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET); 
 	},
 	
 }
--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,1 +1,3 @@
 pref("plasma5opt.colorScheme", "plasma5");
+pref("plasma5opt.labelTextColor", false);
+pref("plasma5opt.labelTextColor_att",'#3D3D3D');
