748b8422111a41537427af24f5601b200281b702
Necip Kafadar <necipkafadar@gmail.com>
2016-05-05 18:16:11 +0300

    preferences bug fix

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -4,5 +4,5 @@ pref("plasma5opt.labelTextColor", false);
 pref("plasma5opt.labelTextColor_att",'#3D3D3D');
 pref("plasma5opt.iconColor", false);
 pref("plasma5opt.iconColor_att", '#000000');
-pref("plasma5opt.tabMinHeight", "100");
-pref("plasma5opt.tabMinWidth", "100");
+pref("plasma5opt.tabMinHeight", 10);
+pref("plasma5opt.tabMinWidth", 100);
